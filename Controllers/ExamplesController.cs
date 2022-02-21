66ad1ae3abf3a7777a1219f83269c31da4a50053
thatbr4d <thatbr4d@gmail.com>
2022-02-21 15:31:35 -0500

    add example files and basic start template

---
+++ Controllers/ExamplesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using dotnet6_contenttype_ajax_examples.Models;
+
+namespace dotnet6_contenttype_ajax_examples.Controllers;
+
+public class ExamplesController : Controller
+{
+    public ExamplesController() { }
+
+    public IActionResult FormEncoded()
+    {
+        return View();
+    }
+
+    public IActionResult JSONEncoded()
+    {
+        return View();
+    }
+}
