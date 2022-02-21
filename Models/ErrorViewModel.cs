a3c95a6ddaf6cfbdd0addaa49984efb18aa1f482
thatbr4d <thatbr4d@gmail.com>
2022-02-21 14:47:25 -0500

    add dotnet 6 mvc base project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnet6_contenttype_ajax_examples.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
